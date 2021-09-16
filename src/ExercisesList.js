5d814da1c0a9ccb56a2f6ca4c5cd09d3c653803a
tundeph <54335828+tundeph@users.noreply.github.com>
2021-09-16 03:58:54 +0100

    Implementing Delete function

--- src/ExercisesList.js
+++ src/ExercisesList.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 function ExercisesList() {
-  return <div>This is Exercises List </div>;
+  const [listExercises, setListExercises] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/exercises/", {})
+      .then((res) => setListExercises(res.data))
+      .catch((err) => console.log(err));
+  });
+
+  const deleteExercise = (id) => {
+    axios
+      .delete("localhost:5000/exercises/" + id)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+    // console.log(id);
+  };
+
+  return (
+    <div>
+      <h2> Exercises Dashboard </h2>
+      <div>
+        {listExercises.map((data) => {
+          return (
+            <div className="flex" key={data._id}>
+              <div> {data._id} </div>
+              <div> {data.username} </div>
+              <div> {data.description} </div>
+              <div> {data.duration} </div>
+              <div> {Date.parse(data.date)} </div>
+              <div>
+                <Link to="" onClick={() => deleteExercise(data._id)}>
+                  Delete
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ExercisesList;
