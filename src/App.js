4ad646a5b898b2322e3007107bc3e844e8326afd
tundeph <54335828+tundeph@users.noreply.github.com>
2022-02-27 20:30:37 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ExercisesList from "./ExercisesList";
-import EditExercise from "./EditExercise";
-import CreateExercise from "./CreateExercise";
-import CreateUser from "./CreateUser";
-import ProtectedRoute from "./ProtectedRoute";
-import Login from "./Login";
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "react-query"
+import ExercisesList from "./ExercisesList"
+import EditExercise from "./EditExercise"
+import CreateExercise from "./CreateExercise"
+import CreateUser from "./CreateUser"
+import ProtectedRoute from "./ProtectedRoute"
+import Login from "./Login"
 
-import "./App.css";
+import "./App.css"
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <div>
-      <Router>
-        <Route path="/" exact component={Login} />
-        <ProtectedRoute path="/list" exact component={ExercisesList} />
-        <ProtectedRoute path="/create" exact component={CreateExercise} />
-        <ProtectedRoute path="/edit/:id" exact component={EditExercise} />
-        <ProtectedRoute path="/user" exact component={CreateUser} />
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Route path="/" exact component={Login} />
+          <ProtectedRoute path="/list" exact component={ExercisesList} />
+          <ProtectedRoute path="/create" exact component={CreateExercise} />
+          <ProtectedRoute path="/edit/:id" exact component={EditExercise} />
+          <ProtectedRoute path="/user" exact component={CreateUser} />
+        </Router>
+      </QueryClientProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
