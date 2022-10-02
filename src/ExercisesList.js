577b694523433505941159fd85b9e028a0b24c67
tundeph <54335828+tundeph@users.noreply.github.com>
2022-10-02 20:33:56 +0100

    Revert to old version

--- src/ExercisesList.js
+++ src/ExercisesList.js
@@ -1,62 +1,87 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Exercise from "./Exercise";
-import { withRouter } from "react-router-dom";
-import { Line } from "react-chartjs-2";
-import MyNavBar from "./MyNavBar";
-import "./ExerciseList.css";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { useQuery } from "react-query"
+import { withRouter } from "react-router-dom"
+
+//components
+import MyNavBar from "./MyNavBar"
+import Exercise from "./Exercise"
+import Chart from "./components/Chart"
+
+//styles
+import "./ExerciseList.css"
 
 function ExercisesList() {
-  const [listFigures, setListFigures] = useState([]);
+  const [listFigures, setListFigures] = useState([])
 
-  useEffect(() => {
-    showExercises();
-  }, []);
+  const fetchExerciseData = async () => {
+    const res = await fetch("https://trackfit-tundeph.herokuapp.com/exercises/")
+    return res.json()
+  }
 
-  // const showExercises = () => {
-  //   axios
-  //     .get("exercises/", {})
-  //     .then((res) => {
-  //       setListFigures(res.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+  const { data, status } = useQuery("exerciseData", fetchExerciseData)
+  console.log(data)
 
-  const showExercises = async () => {
-    try {
-      let fig = await axios.get("exercises/");
-      setListFigures(fig.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // useEffect(() => {
+  //   showExercises()
+  // }, [])
 
-  let running = 0;
-  let swimming = 0;
-  let walking = 0;
-  let cycling = 0;
-  let count = 1;
-  const runningArray = [0];
-  const swimmingArray = [0];
-  const walkingArray = [0];
-  const cyclingArray = [0];
-  const labelsArray = [0];
+  // const showExercises = async () => {
+  //   try {
+  //     let fig = await axios.get("exercises/")
+  //     setListFigures(fig.data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
-  listFigures.forEach((data) => {
-    labelsArray.push(count);
-    count++;
-    walking += data.walking;
-    walkingArray.push(data.walking);
-    running += data.running;
-    runningArray.push(data.running);
-    cycling += data.cycling;
-    cyclingArray.push(data.cycling);
-    swimming += data.swimming;
-    swimmingArray.push(data.swimming);
-  });
+  let running = 0,
+    swimming = 0,
+    walking = 0,
+    cycling = 0
 
+  const runningArray = [0]
+  const swimmingArray = [0]
+  const walkingArray = [0]
+  const cyclingArray = [0]
+  const labelsArray = [0]
+
+  if (data) {
+    data.forEach((data) => {
+      walking += data.walking
+      walkingArray.push(data.walking)
+      running += data.running
+      runningArray.push(data.running)
+      cycling += data.cycling
+      cyclingArray.push(data.cycling)
+      swimming += data.swimming
+      swimmingArray.push(data.swimming)
+    })
+  }
+
+  const exercises = [
+    { name: "Walking", colour: "#f98806", data: walkingArray },
+    { name: "Cycling", colour: "#9668f7", data: cyclingArray },
+    { name: "Running", colour: "#72e294", data: runningArray },
+    { name: "Swimming", colour: "#98c5ff", data: swimmingArray },
+  ]
+
+  const datasets = []
+  for (const [key, value] of Object.entries(exercises)) {
+    datasets.push({
+      label: key,
+      data: value,
+      fill: false,
+      lineTension: 0.5,
+      pointRadius: 0,
+      backgroundColor: "#f98806",
+      borderColor: "#f98806",
+      borderWidth: 2,
+      animation: false,
+    })
+  }
   const graphdata = {
-    labels: labelsArray,
+    labels: Array.from(Array(walkingArray.length).keys()),
     datasets: [
       {
         label: "Walking",
@@ -67,6 +92,7 @@ function ExercisesList() {
         backgroundColor: "#f98806",
         borderColor: "#f98806",
         borderWidth: 2,
+        animation: false,
       },
       {
         label: "Cycling",
@@ -77,6 +103,7 @@ function ExercisesList() {
         backgroundColor: "#9668f7",
         borderColor: "#9668f7",
         borderWidth: 2,
+        animation: false,
       },
       {
         label: "Running",
@@ -87,6 +114,7 @@ function ExercisesList() {
         backgroundColor: "#72e294",
         borderColor: "#72e294",
         borderWidth: 2,
+        animation: false,
       },
       {
         label: "Swimming",
@@ -97,51 +125,40 @@ function ExercisesList() {
         backgroundColor: "#98c5ff",
         borderColor: "#98c5ff",
         borderWidth: 2,
+        animation: false,
       },
     ],
-  };
+  }
 
   return (
     <div className="ExerciseList">
-      <MyNavBar LoggedIn={true} />
-      <div className="container">
-        <h2> All Fitness Data </h2>
-        <div className="col-md-4  mt-5">
-          <select className="form-control">
-            <option>All Time </option>
-            <option>Custom Date </option>
-          </select>
-        </div>
+      {data && (
+        <>
+          <MyNavBar LoggedIn={true} />
+          <div className="container">
+            <h2> All Fitness Data </h2>
+            <div className="col-md-4  mt-5">
+              <select className="form-control">
+                <option>All Time </option>
+                <option>Custom Date </option>
+              </select>
+            </div>
 
-        <div className=" white__box__graph mt-4">
-          <Line
-            data={graphdata}
-            width={300}
-            height={150}
-            options={{
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-              maintainAspectRatio: true,
-            }}
-          />
-        </div>
+            <div className=" white__box__graph mt-4">
+              <Chart data={graphdata} width={300} height={150} />
+            </div>
 
-        <div className="routine__div mt-5">
-          <Exercise routine="Walking" duration={walking} />
-          <Exercise routine="Cycling" duration={cycling} />
-          <Exercise routine="Running" duration={running} />
-          <Exercise routine="Swimming" duration={swimming} />
-        </div>
-      </div>
+            <div className="routine__div mt-5">
+              <Exercise routine="Walking" duration={walking} />
+              <Exercise routine="Cycling" duration={cycling} />
+              <Exercise routine="Running" duration={running} />
+              <Exercise routine="Swimming" duration={swimming} />
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  );
+  )
 }
 
-export default withRouter(ExercisesList);
+export default withRouter(ExercisesList)
